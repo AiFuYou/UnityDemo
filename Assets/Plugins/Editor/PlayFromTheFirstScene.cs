8f6e0ab213a4c781160dcbbf3dc4947b6e2476cc
suyongfu <suyongfu.ifeel@gmail.com>
2020-07-09 23:25:28 +0800

    add fps and PlayFromFirstScene

---
+++ Assets/Plugins/Editor/PlayFromTheFirstScene.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class PlayFromTheFirstScene
+ {      
+     const string playFromFirstMenuStr = "File/Always Start From Scene 0 &p";
+ 
+     static bool playFromFirstScene
+     {
+         get{return EditorPrefs.HasKey(playFromFirstMenuStr) && EditorPrefs.GetBool(playFromFirstMenuStr);}
+         set{EditorPrefs.SetBool(playFromFirstMenuStr, value);}
+     }
+ 
+     [MenuItem(playFromFirstMenuStr, false, 156)]
+     static void PlayFromFirstSceneCheckMenu() 
+     {
+         playFromFirstScene = !playFromFirstScene;
+         Menu.SetChecked(playFromFirstMenuStr, playFromFirstScene);
+ 
+         ShowNotifyOrLog(playFromFirstScene ? "Play from scene 0" : "Play from current scene");
+     }
+ 
+     // The menu won't be gray out, we use this validate method for update check state
+     [MenuItem(playFromFirstMenuStr, true)]
+     static bool PlayFromFirstSceneCheckMenuValidate()
+     {
+         Menu.SetChecked(playFromFirstMenuStr, playFromFirstScene);
+         return true;
+     }
+ 
+     // This method is called before any Awake. It's the perfect callback for this feature
+     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)] 
+     static void LoadFirstSceneAtGameBegins()
+     {
+         if(!playFromFirstScene)
+             return;
+ 
+         if(EditorBuildSettings.scenes.Length == 0)
+         {
+             Debug.LogWarning("The scene build list is empty. Can't play from first scene.");
+             return;
+         }
+ 
+         foreach(GameObject go in Object.FindObjectsOfType<GameObject>())
+             go.SetActive(false);
+         
+         SceneManager.LoadScene(0);
+     }
+ 
+     static void ShowNotifyOrLog(string msg)
+     {
+         if(Resources.FindObjectsOfTypeAll<SceneView>().Length > 0)
+             EditorWindow.GetWindow<SceneView>().ShowNotification(new GUIContent(msg));
+         else
+             Debug.Log(msg); // When there's no scene view opened, we just print a log
+     }
+ }
---
+++ Assets/Scripts/FpsShow.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using UnityEngine;
+
+public class FpsShow : MonoBehaviour
+{
+    public float updateInterval = 0.5f;
+    private float _lastInterval;
+    private int _frames;
+    private float _fps;
+    private GUIStyle _fpsStyle;
+    void Start(){
+        //设置帧率
+        Application.targetFrameRate = 60;
+        _lastInterval = Time.realtimeSinceStartup;
+        _frames = 0;
+        _fpsStyle = new GUIStyle {fontSize = 100};
+    }
+    
+    // Update is called once per frame  
+    void Update(){
+        ++_frames;
+        var timeNow = Time.realtimeSinceStartup;
+        if (timeNow >= _lastInterval + updateInterval)
+        {
+            _fps = _frames / (timeNow - _lastInterval);
+            _frames = 0;
+            _lastInterval = timeNow;
+        }
+    }
+    
+    void OnGUI(){
+        GUI.Label(new Rect(50, 50, 100, 100), _fps.ToString(CultureInfo.InvariantCulture), _fpsStyle);
+    }
+}
